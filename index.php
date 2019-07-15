78e54e95cc9f72c6d3bdce08ba2ac807810bc7e4
lvijudsky <contato@lvijudsky.com.br>
2019-07-15 12:16:50 -0300

    Feito até a questão 10

--- index.php
+++ index.php
@@ -9,7 +9,7 @@
 <body>
     <ol>
         <li>
-            <p>Utilizando um for, imprimir os números de 1 a 100</p>
+            <p>Questão 1 - Utilizando um for, imprimir os números de 1 a 100</p>
             <div>
                 <?php 
                     for ($i=1; $i <= 100 ; $i++) { 
@@ -19,7 +19,7 @@
             </div>
         </li>
         <li>
-            <p>Modificar o exercício anterior para que, em vez de parar no número 100, pare
+            <p>Questão 2 - Modificar o exercício anterior para que, em vez de parar no número 100, pare
 em um número gerado aleatoriamente entre 0 e 100.
 </p>
             <div>
@@ -51,7 +51,7 @@ em um número gerado aleatoriamente entre 0 e 100.
             </div>
         </li>
         <li>
-            <p>Utilizando um while, fazer um programa que lance uma moeda (escolhendo
+            <p>Questão 3 - Utilizando um while, fazer um programa que lance uma moeda (escolhendo
 um número aleatório que pode ser 0 ou 1) até tirar 5 vezes cara (o número 1).
 Ao terminar, imprimir o número de lançamentos da moeda até tirar 5 vezes
 cara.
@@ -81,6 +81,163 @@ cara.
 
                
             </div>
+        </li>
+        <li>
+            <p>Questão 4 - Utilizando um do/while, realizar um programa que lance a moeda até tirar
+cara (o número 1). No final do programa, imprimir quantos lançamentos da
+moeda forem necessários.</p>
+
+            <div>
+            
+            <p>Questão 5 </p>
+
+               
+            </div>
+        </li>
+        <li>
+            <p>Questão 6 - Definir um array com 5 strings que sejam nomes. Percorrer esse array para
+imprimir todos os nomes na tela.
+a. Resolver este problema com um for.
+b. Resolver este problema com while.
+c. Resolver este problema com um do/while
+
+</p>
+            <div>
+            <?php 
+              $nomes = [
+                  'Sérgio',
+                  'Thomaz',
+                  'Fernando Finado',
+                  'Ana Lívia',
+                  'Marcuso!'
+                  
+              ];
+
+              for ($i =  0 ;  $i < count ($nomes) ; $i++) { 
+                 echo($nomes[$i]. '<br>');
+              }
+
+              echo('--<br>');
+              
+              $contador = 0;
+              while ($contador < count($nomes)){
+                echo($nomes[$contador]  .  '<br>');
+                $contador++;
+              }
+
+              echo('--<br>');
+              
+              $contador = 0;
+              do { echo($nomes[$contador] . '<br>');
+                $contador++;
+                
+                  
+              } while ($contador < count($nomes));
+
+
+
+
+
+
+            ?>
+
+
+               
+            </div>
+        </li>
+        <li>
+            <p>Questão 7 - Definir um array com 10 números aleatórios entre 0 e 10. Percorrer esse array
+para imprimir todos os números. A execução deve terminar se algum dos
+números encontrados for 5 (a mensagem impressa deve ser “Encontramos um 5!”).
+a. Resolver este problema com um for.
+b. Resolver este problema com um while.
+c. Resolver este problema com um do/while
+
+</p>
+            <div>
+            <?php 
+
+
+            ?>
+
+
+               
+            </div>
+        </li>
+        <li>
+            <p>Questão 8 - Utilizando a função “Range”(http://php.net/manual/en/function.range.php),
+criar um intervalo de letras (de a até o), imprimir um valor que diga “Na posição
+[index], está o valor [value]”, em que [value] será substituído por cada valor do
+array e [index] representa o índice.</p>
+            
+        <pre>
+            
+            <?php 
+            $letras = range('a', 'o');
+            print_r($letras);
+            ?>  
+        </pre>
+        <div>
+        <?php 
+        foreach ($letras as $posicao => $letra) {
+            # code...
+        }
+        ?>
+        </div>
+        </li>
+        <li>
+            <p>Questão 9 - Definir uma variável $mascote que seja um array associativo
+a. No índice animal, deve dizer que animal é.
+b. No índice idade, deve dizer a idade.
+c. No índice altura, deve dizer a altura.
+d. No índice nome, deve dizer o nome
+</p>
+            
+        <pre>
+            
+            <?php 
+            $mascote = [
+                'animal' =>'Cachorro',
+                'idade' => 4,
+                'altura' => 40,
+                'nome' => 'Rex'
+            ];
+            echo('<pre>');
+            print_r($mascote);
+            echo('</pre>');
+            ?>  
+        </pre>
+        <div>
+        <?php 
+        foreach ($letras as $posicao => $letra) {
+            # code...
+        }
+        ?>
+        </div>
+        </li>
+        <li>
+            <p>Percorrer os valores do array com um foreach que imprima (por exemplo):
+               animal: cachorro
+               idade: 5
+               altura: 0,60
+               nome: Sonic
+</p>
+            
+        <pre>
+            
+            <?php 
+            foreach ($mascote as $posicao => $animal) {
+                echo ($posicao . $animal . '<br>');
+            }
+            ?>  
+        </pre>
+        <div>
+        <?php 
+        foreach ($letras as $posicao => $letra) {
+            # code...
+        }
+        ?>
+        </div>
         </li>
     </ol>
     
